a59ea766717b14e330691106f2c3afcb78f68b5c
Rachel Eiting <reiting1@gmail.com>
2017-03-20 09:37:12 -0500

    lab completed

--- happenTemplate.js
+++ happenTemplate.js
@@ -195,8 +195,8 @@ var library = (function () {
                     var start = new Date(now.getFullYear(), 0, 0);
                     var diff = now - start;
                     var oneDay = 1000 * 60 * 60 * 24;
-                    var day = Math.floor(diff / oneDay);
-                    return String(Math.round(day / 7)+1);
+                    var day = Math.ceil(diff / oneDay);
+                    return String(Math.round(day / 7));
                     ;
                 }
             }
